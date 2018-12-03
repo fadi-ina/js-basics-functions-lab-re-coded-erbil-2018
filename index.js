50d1ec3401c4beb18454b461ab2a08f5a8fa3b09
fadi ina <fadiina610@gmail.com>
2018-12-03 13:11:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+//function describe(){ return document.write("this is node js ")}
+function distanceFromHqInBlocks(\)
+{ return  }
+function distanceTravelledInFeet(point_a,point_b){
+const BLOCK_LENGTH = 264;
+  return Math.abs(point_a-point_b) * BLOCK_LENGTH;
+}
